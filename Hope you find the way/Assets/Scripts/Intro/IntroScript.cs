461b4f97505e0bc7941592af43d6766a3274eff2
Katty Lazar <katy.lazar05@gmail.com>
2022-03-12 21:51:30 +0200

    added transition to images and text on intro

--- Hope you find the way/Assets/Scripts/Intro/IntroScript.cs
+++ Hope you find the way/Assets/Scripts/Intro/IntroScript.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using TMPro;
+using DG.Tweening;
 
 public class IntroScript : MonoBehaviour
 {
 
     [SerializeField] private List<Image> intro_images = new List<Image>();
+    [SerializeField] private TextMeshProUGUI current_phrase;
     private int image_index = -1;
+    private int phrase_index = -1;
+
+    private string[] phrases = {
+        "Anul 2040...",
+        "viata pe pământ este acum un coșmar devenit realitate.",
+        "în ultimul an virusul a preluat frâiele conducerii planetei cauzând câteva miliarde de morți",
+        "dacă exista o persoana care poate găsi leacul, tu ești aceea. trebuie sa duci la capăt misiunea începută de tatăl tău, dar ai grija nimic nu e ceea ce pare."
+    };
+
+    private void Start() {
+        Debug.Log(phrases[1]);
+    }
 
     private string nextScene = "CharacterSelection";
     public void SkipIntro() {
@@ -17,14 +31,22 @@ public class IntroScript : MonoBehaviour
     }
 
     public void NextImage() {
+        current_phrase.DOFade(0, 0);
+
         if ( image_index == intro_images.Count - 1 ){
             SkipIntro();
             return;
         }
 
         image_index += 1;
-        var tempColor = intro_images[image_index].color;
-        tempColor.a = 1;
-        intro_images[image_index].color = tempColor;
+        // var tempColor = intro_images[image_index].color;
+        // tempColor.a = 1;
+        // intro_images[image_index].color = tempColor;
+
+        intro_images[image_index].DOFade(1, 0.5f);
+
+        phrase_index += 1;
+        current_phrase.text = phrases[phrase_index];
+        current_phrase.DOFade(1, 1);
     }
 }
