206ecaf91953e2e5e043c23384951088e12f8747
Katty Lazar <katy.lazar05@gmail.com>
2022-04-27 14:34:45 +0300

    implemented laboratory scene and functionality

---
+++ Hope you find the way/Assets/LABORATORY/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private float moveSpeed;
+
+    private Vector2 moveDirection;
+
+    void Update() {
+        ProcessInputs();
+    }
+
+    void FixedUpdate() {
+        Move();
+    }
+
+    void ProcessInputs() {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2( moveX, moveY ).normalized;
+    }
+
+    void Move() {
+        body.velocity = new Vector2( moveDirection.x * moveSpeed, moveDirection.y * moveSpeed );
+    }
+}
---
+++ Hope you find the way/Assets/LABORATORY/Scripts/PotionMakingManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class PotionMakingManager : MonoBehaviour
+{
+    [SerializeField] private GameObject potionBoiler;
+    [SerializeField] private List<GameObject> ingredients;
+
+    public void MakePotion() {
+        print("fffa");
+        potionBoiler.gameObject.GetComponent<Rigidbody2D>().DORotate( 360f, 2f );
+
+        for ( int i = 0; i < ingredients.Count; i++ ){
+            if ( !ingredients[i].activeSelf ) {
+                ingredients[i].SetActive( true );
+                return; // comment
+            }
+        }
+    }
+}
